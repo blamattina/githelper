2094b0a1161d6192956b5fde6e586c171728afd6
Brian Lamattina <brian@lamattina.io>
2022-01-10 20:38:43 -0500

    Add Material UI + Basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import PrTable from './PrTable';
+import AuthorInput from './AuthorInput';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const VIEWER_LOGIN = gql`
   query {
@@ -13,19 +15,37 @@ const VIEWER_LOGIN = gql`
 `;
 
 function App() {
+  const [author, setAuthor] = useState('blamattina');
   const { data, loading, error } = useQuery(VIEWER_LOGIN);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>ERROR! {error.message}</div>;
 
   return (
-    <div className="App">
-      <div>
-        Authenticated as {data.viewer.login} on{' '}
-        {process.env.REACT_APP_GITHUB_API_URL}
-      </div>
-      <PrTable authors={[data.viewer.login]} />
-    </div>
+    <>
+      <CssBaseline />
+      <Container
+        maxWidth={false}
+        sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: 1,
+          }}
+        >
+          <AuthorInput author={author} setAuthor={setAuthor} />
+          <Box sx={{ alignSelf: 'center' }}>
+            Authenticated as {data.viewer.login} on{' '}
+            {process.env.REACT_APP_GITHUB_API_URL}
+          </Box>
+        </Box>
+        <Box sx={{ flexGrow: 1 }}>
+          <PrTable authors={[author]} />
+        </Box>
+      </Container>
+    </>
   );
 }
 
--- src/PrTable.tsx
+++ src/PrTable.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import usePrData from './hooks/use-pr-data';
 import { PullRequest, SearchResultItemEdge } from './generated/types';
-import PrTableRow from './PrTableRow';
+import { DataGrid } from '@mui/x-data-grid';
 
 type Props = {
-  authors: string[]
-}
+  authors: string[];
+};
 
 function PrTable({ authors }: Props) {
   const { data, loading, error } = usePrData(authors);
@@ -13,13 +13,33 @@ function PrTable({ authors }: Props) {
   if (loading) return <div>Loading...</div>;
   if (error) return <div>ERROR! {error.message}</div>;
 
-  return (
-    <table className="PrTable">
-        {data.search.edges.map(({ node }: SearchResultItemEdge) => {
-          return <PrTableRow pullRequest={node as PullRequest} />
-        })}
-    </table>
-  );
+  const columns = [
+    { field: 'createdAt', headerName: 'Created At', width: 200 },
+    { field: 'author', headerName: 'author' },
+    { field: 'number', headerName: 'PR Number' },
+    { field: 'title', headerName: 'PR Title', width: 500 },
+    { field: 'state', headerName: 'State' },
+    { field: 'changedFiles', headerName: 'Changed Files' },
+    { field: 'additions', headerName: 'Additions' },
+    { field: 'deletions', headerName: 'Deletions' },
+  ];
+
+  const rows = data.search.edges.map(({ node }: SearchResultItemEdge) => {
+    const pullRequest = node as PullRequest;
+    console.log(pullRequest);
+    return {
+      id: pullRequest.id,
+      createdAt: pullRequest.createdAt,
+      author: pullRequest.author.login,
+      title: pullRequest.title,
+      number: pullRequest.number,
+      additions: pullRequest.additions,
+      deletions: pullRequest.deletions,
+      changedFiles: pullRequest.changedFiles,
+      state: pullRequest.state,
+    };
+  });
+  return <DataGrid columns={columns} rows={rows} rowsPerPageOptions={[50]} />;
 }
 
 export default PrTable;
