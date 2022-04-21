91103d748581bacd7cf8fa6fbdd73c4dfcd42a57
Brian Lamattina <brian@lamattina.io>
2022-04-21 17:43:05 -0400

    Create a proxy server to proxy requests to github api

--- client/src/common/apollo-client.ts
+++ client/src/common/apollo-client.ts
@@ -2,10 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: `${process.env.REACT_APP_GITHUB_API_URL}/graphql`,
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_AUTH_TOKEN}`,
-  },
+  uri: `/github/graphql`,
 });
 
 export default client;
