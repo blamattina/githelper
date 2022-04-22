78dd6efc3b77ff3bde21f695a520b03327d87f2c
Brian Lamattina <brian@lamattina.io>
2022-04-22 13:21:28 -0400

    Remove team stuff for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,19 @@
 import React, { useState, useCallback } from 'react';
-import { useQuery, gql } from '@apollo/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import UserSearch from './UserSearch';
-import TeamSearch from './TeamSearch';
 import Contributions from './Contributions';
-import TeamContributions from './TeamContributions';
 
 type AuthorOption = {
   label: string;
   login: string;
 };
 
-type TeamOption = {
-  label: string;
-  slug: string;
-  members: string[];
-};
-
 function App() {
   const [authors, setAuthors] = useState<AuthorOption[]>([]);
-  const [teams, setTeams] = useState<TeamOption[]>([]);
   const [startDate, setStartDate] = useState<Date>(new Date());
   const [endDate, setEndDate] = useState<Date>(new Date());
 
@@ -64,11 +54,6 @@ function App() {
               onChange={setAuthors}
               sx={{ marginRight: 1 }}
             />
-            <TeamSearch
-              label="Search by Team"
-              onChange={setTeams}
-              sx={{ marginRight: 1 }}
-            />
           </Box>
           <DesktopDatePicker
             label="Start Date"
@@ -90,16 +75,6 @@ function App() {
             <Contributions
               key={author.login}
               authors={[author.login]}
-              startDate={startDate}
-              endDate={endDate}
-            />
-          ))}
-          {teams.map((team) => (
-            <TeamContributions
-              key={team.slug}
-              organization="HubSpot"
-              teamSlug={team.slug}
-              authors={team.members}
               startDate={startDate}
               endDate={endDate}
             />
