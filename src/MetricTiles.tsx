3c81e765e77645da47ca3391add7714129903a56
Brian Barbosa <bbarbosa@hubspot.com>
2022-05-02 17:40:57 -0400

    Tweaks based on feedback
    Two tweaks based on feedback:
    * Changed metric tiles to color Black.
    * Removed additions/deletions.

--- src/MetricTiles.tsx
+++ src/MetricTiles.tsx
@@ -15,7 +15,7 @@ const Tile = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: 'center',
-  color: '#2da44e',
+  color: '#000000',
   height: '100%',
 }));
 
--- src/PullCreationChart.tsx
+++ src/PullCreationChart.tsx
@@ -2,14 +2,13 @@ import format from 'date-fns/format';
 import startOfWeek from 'date-fns/startOfWeek';
 import { Paper } from '@mui/material';
 import {
-  Bar,
-  ComposedChart,
+  LineChart,
   Legend,
   Line,
-  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
+  ResponsiveContainer,
 } from 'recharts';
 import { PullRequestKeyMetrics } from './types';
 import { addWeeks } from 'date-fns';
@@ -90,44 +89,29 @@ function PullCreationChart({ pullRequests, startDate, endDate }: Props) {
   }
   data.sort();
 
+  console.log(data);
+
   return (
     <Paper elevation={0} sx={{ height: '100%' }}>
       <ResponsiveContainer height={350}>
-        <ComposedChart data={data}>
+        <LineChart data={data}>
           <XAxis dataKey="weekString" scale="band" />
-          <YAxis yAxisId="left-axis" />
-          <YAxis yAxisId="right-axis" orientation="right" />
+          <YAxis />
           <Tooltip />
           <Legend />
-          <Bar
-            name="Added Lines of Code"
-            yAxisId="left-axis"
-            dataKey="additions"
-            stackId="linesofcode"
-            fill="#2da44e"
-          />
-          <Bar
-            name="Deleted Lines of Code"
-            yAxisId="left-axis"
-            dataKey="deletions"
-            stackId="linesofcode"
-            fill="#cf222e"
-          />
           <Line
             name="New Pulls"
-            yAxisId="right-axis"
             type="monotone"
             dataKey="pullsCreated"
             stroke="#ff5c35"
           />
           <Line
             name="Pulls Merged"
-            yAxisId="right-axis"
             type="monotone"
             dataKey="pullsMerged"
             stroke="#8250df"
           />
-        </ComposedChart>
+        </LineChart>
       </ResponsiveContainer>
     </Paper>
   );
