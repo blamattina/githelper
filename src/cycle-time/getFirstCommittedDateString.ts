ea5582a1dc2f758e42238d7b11d70fd7d7eb4146
Brian Lamattina <brian@lamattina.io>
2022-05-09 15:03:01 -0400

    clean up getFirstCommittedDateString

--- src/cycle-time/getFirstCommittedDateString.ts
+++ src/cycle-time/getFirstCommittedDateString.ts
@@ -1,9 +1,7 @@
-import takeWhile from 'lodash/takeWhile';
-import keyBy from 'lodash/keyBy';
 import { PullRequestCommit, PullRequest } from '../generated/types';
 
-export default function getFirstCommittedDateString(pullRequest: PullRequest): string {
-  // Find the first commit node in the timeline.
+// Find the first commit in the PR and return its committed date string
+export default function getFirstCommittedDateString(pullRequest: PullRequest): string | undefined {
   const firstCommitNode = pullRequest.timelineItems.edges.find(
     ({ node }) => 'commit' in node
   )?.node as PullRequestCommit | undefined;
