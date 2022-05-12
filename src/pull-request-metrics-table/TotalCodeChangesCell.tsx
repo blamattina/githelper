1cbb3f4ba5e54bf75228b33adb2d8ff42e53b4e0
Brian Lamattina <brian@lamattina.io>
2022-05-11 20:08:20 -0400

    Move asterisk into the tooltip

--- src/pull-request-metrics-table/TotalCodeChangesCell.tsx
+++ src/pull-request-metrics-table/TotalCodeChangesCell.tsx
@@ -15,29 +15,27 @@ const TotalCodeChangesCell: React.FC<Props> = ({
   totalCodeChanges,
 }) => {
   return (
-    <>
-      <Tooltip
-        title={
-          <span>
-            Total Code Changes: {totalCodeChanges}
-            {totalCodeChanges >= PULL_REQUEST_CHANGESET_LIMIT && (
-              <p>
-                *This PR is quite large and likely includes moved or generated
-                files. It is not included in any roll-ups that count total code
-                changes.
-              </p>
-            )}
-          </span>
-        }
-        enterDelay={1000}
-      >
+    <Tooltip
+      title={
         <span>
-          <span style={{ color: green[500] }}>++{additions}</span>
-          <span style={{ color: red[500] }}>--{deletions}</span>
+          Total Code Changes: {totalCodeChanges}
+          {totalCodeChanges >= PULL_REQUEST_CHANGESET_LIMIT && (
+            <p>
+              *This PR is quite large and likely includes moved or generated
+              files. It is not included in any roll-ups that count total code
+              changes.
+            </p>
+          )}
         </span>
-      </Tooltip>
-      {totalCodeChanges >= PULL_REQUEST_CHANGESET_LIMIT && <strong>*</strong>}
-    </>
+      }
+      enterDelay={1000}
+    >
+      <span>
+        <span style={{ color: green[500] }}>++{additions}</span>
+        <span style={{ color: red[500] }}>--{deletions}</span>
+        {totalCodeChanges >= PULL_REQUEST_CHANGESET_LIMIT && <strong>*</strong>}
+      </span>
+    </Tooltip>
   );
 };
 
