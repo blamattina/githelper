f6ba05cc57996ef3c5e75ead61df1f2cba4996a8
Brian LaMattina <blamattina@users.noreply.github.com>
2022-05-11 20:09:20 -0400

    Omit massive pull requests from aggregate metrics (#68)
    * Omit massive pull requests from aggregate metrics
    
    * Move asterisk into the tooltip

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const PULL_REQUEST_CHANGESET_LIMIT = 5000; // Lines of code
--- src/pull-request-metrics-table/TotalCodeChangesCell.tsx
+++ src/pull-request-metrics-table/TotalCodeChangesCell.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import { red, green } from '@mui/material/colors';
+import { PULL_REQUEST_CHANGESET_LIMIT } from '../constants';
 
 type Props = {
   additions: number;
@@ -15,12 +16,24 @@ const TotalCodeChangesCell: React.FC<Props> = ({
 }) => {
   return (
     <Tooltip
-      title={`Total Code Changes: ${totalCodeChanges}`}
+      title={
+        <span>
+          Total Code Changes: {totalCodeChanges}
+          {totalCodeChanges >= PULL_REQUEST_CHANGESET_LIMIT && (
+            <p>
+              *This PR is quite large and likely includes moved or generated
+              files. It is not included in any roll-ups that count total code
+              changes.
+            </p>
+          )}
+        </span>
+      }
       enterDelay={1000}
     >
       <span>
         <span style={{ color: green[500] }}>++{additions}</span>
         <span style={{ color: red[500] }}>--{deletions}</span>
+        {totalCodeChanges >= PULL_REQUEST_CHANGESET_LIMIT && <strong>*</strong>}
       </span>
     </Tooltip>
   );
