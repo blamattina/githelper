836721c315819f3470b0f3b9dc97af828bac19d8
Brian LaMattina <blamattina@users.noreply.github.com>
2022-05-18 18:25:24 -0400

    WIP Display missing scopes and network errors (#72)
    * WIP commit to present missing scopes and network errors
    
    * Fix type error and bug when switching hosts
    
    * Clean up
    
    * Fix lint
    
    * Remove console.log

--- src/auth/GitHubApolloProvider.tsx
+++ src/auth/GitHubApolloProvider.tsx
@@ -1,7 +1,13 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider,
+  ApolloLink,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import { GitHubTokensContext } from './GitHubTokensProvider';
 import { useParams, Navigate, createSearchParams } from 'react-router-dom';
 
@@ -10,32 +16,90 @@ function makeUri(hostname: string): string {
   return `https://${hostname}/api/graphql`;
 }
 
+function findMissingScopes(hostname: string, scopes: string[]): string[] {
+  const requiredScopes = ['read:discussion', 'read:org', 'read:user', 'repo'];
+
+  if (hostname !== 'api.github.com') {
+    requiredScopes.push('read:enterprise');
+  }
+
+  return requiredScopes.reduce((acc, scope: string) => {
+    if (!scopes.includes(scope)) acc.push(scope);
+    return acc;
+  }, [] as string[]);
+}
+
 const GitHubApolloProvider: React.FC = ({ children }) => {
+  const [linkErrorMessage, setLinkErrorMessage] = useState<string | null>(null);
   const { getGitHubToken } = useContext(GitHubTokensContext);
   const { gitHubHostname } = useParams();
 
   const gitHubToken = useMemo(() => {
+    setLinkErrorMessage(null);
     if (!gitHubHostname) return null;
     return getGitHubToken(gitHubHostname);
   }, [gitHubHostname, getGitHubToken]);
 
   const client = useMemo(() => {
     if (!gitHubToken) return null;
-    return new ApolloClient({
-      cache: new InMemoryCache(),
+
+    const httpLink = new HttpLink({
       uri: makeUri(gitHubToken.hostname),
       headers: {
         authorization: `Bearer ${gitHubToken.token}`,
       },
     });
+
+    const errorLink = onError(({ networkError }) => {
+      if (networkError) {
+        setLinkErrorMessage(`Network Error: ${networkError.message}`);
+      }
+    });
+
+    const scopeValidationLink = new ApolloLink((operation, forward) => {
+      return forward(operation).map((response) => {
+        const context = operation.getContext();
+        const {
+          response: { headers },
+        } = context;
+        const scopes = headers.get('x-oauth-scopes').split(', ');
+
+        const missingScopes = findMissingScopes(gitHubToken.hostname, scopes);
+
+        if (missingScopes.length) {
+          setLinkErrorMessage(
+            `Personal Access Token for ${
+              gitHubToken.hostname
+            } is missing required scopes: ${missingScopes.join()}`
+          );
+        }
+
+        return response;
+      });
+    });
+
+    return new ApolloClient({
+      cache: new InMemoryCache(),
+      link: scopeValidationLink.concat(errorLink).concat(httpLink),
+    });
   }, [gitHubToken]);
 
   if (!client) {
-    const searchParams = gitHubHostname ? `?${createSearchParams({ gitHubHostname })}` : '';
+    const searchParams = gitHubHostname
+      ? `?${createSearchParams({ gitHubHostname })}`
+      : '';
     return <Navigate to={`/new${searchParams}`} replace />;
   }
 
-  return <ApolloProvider client={client}><Outlet /></ApolloProvider>;
+  if (linkErrorMessage) {
+    return <div>{linkErrorMessage}</div>;
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <Outlet />
+    </ApolloProvider>
+  );
 };
 
 export default GitHubApolloProvider;
