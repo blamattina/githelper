f9e517ff8fc319b36ae71d7de2431a65e8a9e40e
Brian Barbosa <barbosab@users.noreply.github.com>
2022-05-19 17:05:03 -0400

    Added an empty error state (#73)
    * Added an empty error state
    
    * Added VPN language

--- src/auth/GitHubApolloProvider.tsx
+++ src/auth/GitHubApolloProvider.tsx
@@ -9,6 +9,8 @@ import {
 import { onError } from '@apollo/client/link/error';
 import { GitHubTokensContext } from './GitHubTokensProvider';
 import { useParams, Navigate, createSearchParams } from 'react-router-dom';
+import ErrorTwoToneIcon from '@mui/icons-material/ErrorTwoTone';
+import { Box, Grid, Typography } from '@mui/material';
 
 function makeUri(hostname: string): string {
   if (hostname === 'api.github.com') return 'https://api.github.com/graphql';
@@ -56,7 +58,28 @@ const GitHubApolloProvider: React.FC = ({ children }) => {
   }
 
   if (linkErrorMessage) {
-    return <div>{linkErrorMessage}</div>;
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid xs={4}></Grid>
+          <Grid xs={4} item style={{ textAlign: 'center' }}>
+            <ErrorTwoToneIcon sx={{ fontSize: 250 }} color="error" />
+          </Grid>
+          <Grid xs={4}></Grid>
+          <Grid xs={3}></Grid>
+          <Grid xs={6} style={{ textAlign: 'center' }} item>
+            <Typography variant="h5">
+              {linkErrorMessage}
+              <br />
+              <br />
+              {gitHubHostname !== 'api.github.com' &&
+                'If your company requires VPN access to GitHub Enterprise, check to see if you are on the network.'}
+            </Typography>
+          </Grid>
+          <Grid xs={3}></Grid>
+        </Grid>
+      </Box>
+    );
   }
 
   return (
